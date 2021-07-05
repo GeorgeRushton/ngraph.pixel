b7187e842ab711f33ba2fcfb3b18ac33df9bb27b
grushton11 <george.rushton@dazn.com>
2021-07-05 17:05:19 +0800

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var THREE = require('three');
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 module.exports = pixel;
 
@@ -370,7 +371,7 @@ function pixel(graph, options) {
     input.on('nodeclick', passthrough('nodeclick'));
     input.on('nodedblclick', passthrough('nodedblclick'));
 
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls = new OrbitControls( camera, renderer.domElement );
 
     window.addEventListener('resize', onWindowResize, false);
   }
